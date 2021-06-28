865f827cf745f3b3a937e4417d06efbffe5c8f5f
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-06-28 06:12:49 +0530

    adding the front end

---
+++ blog/client/src/PostList.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import CommentCreater from "./CommentCreate";
+const PostList = () => {
+  const [postList, setPostList] = useState({});
+  const getPostList = async () => {
+    const posts = await axios.get("http://localhost:4000/posts");
+    setPostList(posts.data);
+  };
+
+  useEffect(() => {
+    getPostList();
+  }, []);
+
+  const renderedObjects = Object.values(postList).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentCreater postId={post.id} />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedObjects}
+    </div>
+  );
+};
+
+export default PostList;
