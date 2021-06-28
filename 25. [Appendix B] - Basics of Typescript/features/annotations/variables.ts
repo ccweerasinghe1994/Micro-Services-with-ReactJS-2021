0cc1aec3dcf0f2a145639b4cf44c0d8d27e7f78f
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-06-28 07:24:06 +0530

    Update variables.ts

--- 25. [Appendix B] - Basics of Typescript/features/annotations/variables.ts
+++ 25. [Appendix B] - Basics of Typescript/features/annotations/variables.ts
@@ -12,4 +12,29 @@ let nothing: undefined = undefined;
 
 let now: Date = new Date();
 
+// array
 
+let colors: string[] = ["red", "blue", "green"];
+
+let numbers: number[] = [1, 34, 2, 56];
+
+let trooths: boolean[] = [true, true, false];
+
+// classes
+
+class Car {}
+
+let car: Car = new Car();
+
+// object literlal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// functions
+
+const lognumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
