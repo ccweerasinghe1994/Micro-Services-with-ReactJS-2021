4fce3ba6b7479e4e1d8880f8fe5c4a8bd0cf8ce9
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-06-28 07:01:09 +0530

    type annotations' for an simple variables

---
+++ 25. [Appendix B] - Basics of Typescript/features/annotations/variables.ts
@@ -0,0 +1,15 @@
+let apples: number = 5;
+
+let speed: string = "sdfsdfsf";
+
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+
+let nothing: undefined = undefined;
+
+// built-in objects
+
+let now: Date = new Date();
+
+
